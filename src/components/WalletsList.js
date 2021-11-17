b92ab43f336bbd1481bad9fcf6ba1cee6b66d661
Jaroslaw Ucinski <jaroslaw.ucinski@cshark.com>
2021-11-17 22:19:19 +0100

    Add WalletList

---
+++ src/components/WalletsList.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import ChipsArray from './ChipsArray'
+
+const mockedWallets = [
+  { key: 1, label: 'Retirement' },
+  { key: 2, label: 'Longterm' },
+  { key: 3, label: 'Speculative' },
+  { key: 4, label: 'Model' },
+]
+
+const WalletsList = ({ onChange }) => {
+  const [wallets, setWallets] = useState(mockedWallets);
+  
+  const handleSelectedChange = selected => {
+    if (onChange) {
+      onChange(selected);
+    }
+  }
+
+  return (
+    <div>
+      <ChipsArray initialData={wallets} type="multi" onChange={handleSelectedChange} />
+    </div>
+  )
+}
+
+WalletsList.propTypes = {
+  onChange: PropTypes.func,
+}
+
+export default WalletsList
